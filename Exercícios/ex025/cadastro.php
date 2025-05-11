706c2eb6dfd794cd0b11e457080c85e97b6835b8
Lucas Eduardo Bastos da Silva <lucasbastosozy@gmail.com>
2025-05-11 15:51:27 -0300

    php

---
+++ Exercícios/ex025/cadastro.php
@@ -0,0 +1,9 @@
+Os dados não foram enviados corretamente. Por favor, preencha todos os campos obrigatórios.
+e Aprenda PHP 
+
+if nome >= Auto
+    echo "Nome: $nome <br>";
+ elseif nome < Auto
+// Verifica se o formulário foi enviado{
+    echo "Nome inválido. <br>";
+}
